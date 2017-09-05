a090ca2a3bd3ae015c30c7521417ebdbb4b63580
sorin <s.tiberiu46@yandex.com>
2017-09-05 19:02:52 +0300

    9.5.17 Add DB root password for initial migration

--- config/database.php
+++ config/database.php
@@ -56,7 +56,7 @@ return [
             'driver' => 'mysql',
             'host' => env('DB_HOST', 'localhost'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'RessApp'),
+            'database' => env('DB_DATABASE', 'ressapp'),
             'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', '12345678'),
             'charset' => 'utf8',
