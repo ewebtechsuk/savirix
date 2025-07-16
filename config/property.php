bf9db84f8bda4e3e9a94852684024fb7eb88ff4b
ewebtechs <ewebtechs@gmail.com>
2025-07-16 03:03:23 +0100

    Clean up and modernize project

---
+++ config/property.php
@@ -0,0 +1,43 @@
+<?php
+
+return [
+    'property_types' => [
+        1 => 'A1 Commercial',
+        2 => 'A3 Commercial',
+        3 => 'Apartment',
+        4 => 'Bedsit',
+        5 => 'Commercial Shop',
+        6 => 'Cottage',
+        7 => 'Flat',
+        8 => 'Flat share',
+        9 => 'Hostel',
+        10 => 'Hotel',
+        11 => 'House',
+        12 => 'House share',
+        13 => 'Maisonette',
+        14 => 'Office Space',
+        15 => 'Olympic property',
+        16 => 'Penthouse',
+        17 => 'Studio',
+        18 => 'Villa',
+        19 => 'Warehouse conversion',
+    ],
+
+    'transaction_types' => [
+        1 => 'Sale',
+        2 => 'Let',
+    ],
+
+    'portal_statuses' => [
+        1 => 'Available',
+        2 => 'Under offer',
+        3 => 'Sold stc',
+        4 => 'Let agreed',
+    ],
+
+    'currencies' => [
+        1 => '£',
+        2 => '$',
+        3 => '€',
+    ],
+];
