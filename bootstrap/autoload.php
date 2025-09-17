b6a85529f2579553971852080a72d49a7652f152
ewebtechs <ewebtechs@gmail.com>
2025-09-17 04:06:44 +0100

    Restore cached vendor dependencies before bootstrapping

--- bootstrap/autoload.php
+++ bootstrap/autoload.php
@@ -14,7 +14,52 @@ define('LARAVEL_START', microtime(true));
 |
 */
 
-require __DIR__.'/../vendor/autoload.php';
+$vendorDirectory = __DIR__.'/../vendor';
+$vendorAutoload = $vendorDirectory.'/autoload.php';
+$cachedVendorDirectory = __DIR__.'/../deps/vendor';
+
+if (!function_exists('laravelCopyDirectory')) {
+    function laravelCopyDirectory($source, $destination)
+    {
+        $iterator = new RecursiveIteratorIterator(
+            new RecursiveDirectoryIterator($source, RecursiveDirectoryIterator::SKIP_DOTS),
+            RecursiveIteratorIterator::SELF_FIRST
+        );
+
+        foreach ($iterator as $item) {
+            $targetPath = $destination.DIRECTORY_SEPARATOR.$iterator->getSubPathName();
+
+            if ($item->isDir()) {
+                if (!is_dir($targetPath) && !@mkdir($targetPath, 0775, true) && !is_dir($targetPath)) {
+                    throw new RuntimeException('Unable to create directory: '.$targetPath);
+                }
+            } else {
+                if (!@copy($item->getPathname(), $targetPath)) {
+                    throw new RuntimeException('Unable to copy file: '.$targetPath);
+                }
+            }
+        }
+    }
+}
+
+if (!file_exists($vendorAutoload) && is_dir($cachedVendorDirectory)) {
+    if (is_link($vendorDirectory)) {
+        @unlink($vendorDirectory);
+    }
+
+    if (!is_dir($vendorDirectory) && !@mkdir($vendorDirectory, 0775, true) && !is_dir($vendorDirectory)) {
+        throw new RuntimeException('Unable to create vendor directory.');
+    }
+
+    laravelCopyDirectory($cachedVendorDirectory, $vendorDirectory);
+}
+
+if (file_exists($vendorAutoload)) {
+    require $vendorAutoload;
+} else {
+    fwrite(STDERR, "Composer dependencies are missing. Run 'composer install' or ensure deps/vendor is available.\n");
+    exit(1);
+}
 
 /*
 |--------------------------------------------------------------------------
