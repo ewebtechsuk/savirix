ef625c4e133a6d2fdcf46ebbd1979bc869d3b0cc
ewebtechs <ewebtechs@gmail.com>
2025-09-29 03:41:17 +0100

    Improve phpunit runner autoload check

--- scripts/phpunit-runner.php
+++ scripts/phpunit-runner.php
@@ -1,7 +1,17 @@
 #!/usr/bin/env php
 <?php
 
-require __DIR__ . '/../vendor/autoload.php';
+$autoloadPath = __DIR__ . '/../vendor/autoload.php';
+
+if (!is_file($autoloadPath)) {
+    fwrite(
+        STDERR,
+        "Composer autoload file not found. Please run `composer install` before executing the test suite." . PHP_EOL
+    );
+    exit(1);
+}
+
+require $autoloadPath;
 
 use PHPUnit\TextUI\Application;
 
